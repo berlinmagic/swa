7b674620ef0d3b65e70aa1d73627552e7445ea8b
Torsten Wetzel <torstenwetzel@berlinmagic.com>
2012-08-23 00:51:12 +0200

    initial commit - swa airport

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swa::Application.config.secret_token = '9079525dd7711d3c278a1e856ea6446d2ef8ee70ccfc9b3b9c73768637056c09471667e7a6d9036a2cb763b41309d31c68b465ffa8111944243af64a56a65558'
