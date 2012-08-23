f6548c40427e3d7b902a8f36283142ad541c6948
Torsten Wetzel <torstenwetzel@berlinmagic.com>
2012-08-23 15:48:43 +0200

    first steps to profile
    - add grid-css
    - add bourbon css helper
    - update some views and styles

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ end
 gem 'jquery-rails'
 
 gem "devise", "~> 2.1.2"
+gem "bourbon"
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,6 @@
 class Person < ActiveRecord::Base
+  
+  GENDERS = %w(male female)
+  
   attr_accessible :birthday, :first_name, :name, :nick_name, :proffession, :sex
 end
